e231809de207d9ace58afb8661e0438ad691268e
laurencnc <lilongcnc@vip.qq.com>
2022-07-22 15:10:09 +0800

    增加内容localange

--- Example/Example/ViewController.swift
+++ Example/Example/ViewController.swift
@@ -7,11 +7,23 @@
 
 import UIKit
 
+
+// NSLocalizedString(key, comment) 本质
+//// NSlocalizeString 第一个参数是内容,根据第一个参数去对应语言的文件中取对应的字符串，第二个参数将会转化为字符串文件里的注释，可以传nil，也可以传空字符串@""。
+//#define NSLocalizedString(key, comment) [[NSBundle mainBundle] localizedStringForKey:(key) value:@"" table:nil]
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        var aaa = Bundle.main.localizedString(forKey: "AAA", value:"", table: "")
+        
+        
+        let label = UILabel(frame:CGRect(x:30,y:150,width:200,height:40))
+        label.text = aaa
+        view.addSubview(label)
+        
     }
 
 
